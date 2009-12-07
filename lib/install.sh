b23901d452386aa433a67c1160f2de736def6f55
Paul MERLIN <paul@ceylang.(none)>
2009-12-07 11:40:30 +0100

    initial import

---
+++ lib/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# This script install locally needed artifacts not found in any public maven repository
+
+
+install_jar() {
+	mvn install:install-file -Dfile=${1} -DgroupId=${2} -DartifactId=${3} -Dversion=${4} -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
+	echo "Installed ${1} with GAV: ${2}:${3}:${4}"
+}
+
+
+install_jar "batik-all.jar" "batik" "batik-all" "1.7-with-r608262"
+
+
